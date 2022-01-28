5fa6cb8ad0d11e08e7723a8cc5704361f25dfa38
Garrett Comes <garrett1124@icloud.com>
2022-01-28 07:57:55 -0700

    Switched to Shuffleboard

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -4,34 +4,42 @@
 
 #include "Robot.h"
 
-#include <frc/smartdashboard/SmartDashboard.h>
+#include <frc/shuffleboard/Shuffleboard.h>
+// #include <frc/smartdashboard/SmartDashboard.h>
 #include <frc2/command/CommandScheduler.h>
 
-void Robot::RobotInit() {}
+void Robot::RobotInit() {
+  frc2::CommandScheduler::GetInstance().Enable();
+  frc2::CommandScheduler::GetInstance().OnCommandInitialize(
+      [](const frc2::Command& command) {
+        frc::Shuffleboard::AddEventMarker(
+            "Command Initialized", command.GetName(),
+            frc::ShuffleboardEventImportance::kNormal);
+      });
+  frc2::CommandScheduler::GetInstance().OnCommandInterrupt(
+      [](const frc2::Command& command) {
+        frc::Shuffleboard::AddEventMarker(
+            "Command Interrupted", command.GetName(),
+            frc::ShuffleboardEventImportance::kNormal);
+      });
+  frc2::CommandScheduler::GetInstance().OnCommandFinish(
+      [](const frc2::Command& command) {
+        frc::Shuffleboard::AddEventMarker(
+            "Command Finished", command.GetName(),
+            frc::ShuffleboardEventImportance::kNormal);
+      });
+}
 
-/**
- * This function is called every robot packet, no matter the mode. Use
- * this for items like diagnostics that you want to run during disabled,
- * autonomous, teleoperated and test.
- *
- * <p> This runs after the mode specific periodic functions, but before
- * LiveWindow and SmartDashboard integrated updating.
- */
-void Robot::RobotPeriodic() { frc2::CommandScheduler::GetInstance().Run(); }
+void Robot::RobotPeriodic() {
+  frc2::CommandScheduler::GetInstance().Run();
+}
 
-/**
- * This function is called once each time the robot enters Disabled mode. You
- * can use it to reset any subsystem information you want to clear when the
- * robot is disabled.
- */
-void Robot::DisabledInit() {}
+void Robot::DisabledInit() {
+  frc2::CommandScheduler::GetInstance().Disable();
+}
 
 void Robot::DisabledPeriodic() {}
 
-/**
- * This autonomous runs the autonomous command selected by your {@link
- * RobotContainer} class.
- */
 void Robot::AutonomousInit() {
   // m_autonomousCommand = m_container.GetAutonomousCommand();
 
@@ -53,16 +61,12 @@ void Robot::TeleopInit() {
   //}
 }
 
-/**
- * This function is called periodically during operator control.
- */
 void Robot::TeleopPeriodic() {}
 
-/**
- * This function is called periodically during test mode.
- */
 void Robot::TestPeriodic() {}
 
 #ifndef RUNNING_FRC_TESTS
-int main() { return frc::StartRobot<Robot>(); }
+int main() {
+  return frc::StartRobot<Robot>();
+}
 #endif
